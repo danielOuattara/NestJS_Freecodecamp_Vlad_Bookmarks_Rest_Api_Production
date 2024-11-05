8445dea759cda08c68ef97901fff7a1cd26bc6a6
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-11-05 11:16:32 +0100

    AuthController

--- src/auth/auth.controler.ts
+++ src/auth/auth.controler.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
-@Controller()
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Get()
+  welcome() {
+    return 'Welcome';
+  }
+
+  @Post('login')
+  login() {
+    return this.authService.login();
+  }
+
+  @Post('signup')
+  signup() {
+    return 'signup !';
+  }
 }
